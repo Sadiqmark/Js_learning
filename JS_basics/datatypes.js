e17a97bfa6ba481ffc03372422c0e77efab2399b
Sadiqmark <53846096+Sadiqmark@users.noreply.github.com>
2024-02-09 09:03:17 +0000

    datatypes

---
+++ JS_basics/datatypes.js
@@ -0,0 +1,40 @@
+// data types are
+// number
+// String
+// BigInt
+// Boolean
+// null
+// undefined
+// Symbol 
+// Object
+// these are the data types in js
+//1.numbers
+let a=90
+let b=89.7
+console.log(typeof a, typeof b);
+//2.string
+let c='sadiq'
+let d="khan"
+console.log(c,d)
+console.log(typeof c, typeof d)
+// we can embbed strings for example
+let x="sadiqq"
+console.log('hello,${x}');
+//3.bidint
+let y=BigInt(4848447483837483)
+console.log(y);
+console.log(typeof y);
+//4.boolean
+// returns true or false
+//5.Null
+let v=null
+console.log(typeof v)
+//6.undefined
+let o;
+console.log(typeof o);
+//7.symbol
+let r=Symbol(3456)
+console.log(r)
+let p=Symbol(3456)
+console.log(r==p)
+
