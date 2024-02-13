a2ad6328f20bbd961cb8e712229236a6b6056ec1
Sadiqmark <53846096+Sadiqmark@users.noreply.github.com>
2024-02-13 14:08:21 +0000

    number commit

---
+++ JS_basics/numbers_math.js
@@ -0,0 +1,31 @@
+let score=98
+console.log(score)
+const anotherNum= new Number(67)
+console.log(anotherNum)
+//Tofixed method
+let fixed=679.987
+console.log(fixed.toFixed(3));
+//toPrecision
+let perNum=79.97876
+console.log(perNum.toPrecision(3));
+//tostring
+let stNum=898
+console.log(typeof stNum.toString())
+//some operation in string number
+let a="100"
+let b="10"
+console.log(a-b)
+
+console.log(a+b)
+console.log(a*b)
+console.log(a/b)
+//+++++Math+++++
+
+//abs
+let absNum=-9
+console.log(Math.abs(absNum))
+
+//round
+console.log(Math.round(7.4))
+
+
